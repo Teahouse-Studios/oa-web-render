a8661391c1c9c000404d920cb5030121c899d5e3
yzhh <31803608+OasisAkari@users.noreply.github.com>
2024-04-22 20:08:23 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -362,21 +362,39 @@ app.use(require('body-parser').json({
 
       await page.evaluate((section, elements_to_disable) => {
         const levels = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6']
-        const sec = document.getElementById(section).parentNode
+        let sec = document.getElementById(section).parentNode
         const sec_level = sec.tagName
+        if (sec.parentNode.className.includes('ext-discussiontools-init-section')){ // wo yi ding yao sha le ni men
+          sec = sec.parentNode
+        }
         const nbox = document.createElement('div')
         nbox.className = 'bot-sectionbox'
         nbox.style = 'display: inline-block; padding: 15px'
         nbox.appendChild(sec.cloneNode(true))
         let next_sibling = sec.nextSibling
-        while (true) {
-          if (next_sibling == null) {
-            break
-          }
+        while (next_sibling) {
           if (levels.includes(next_sibling.tagName)) {
             if (levels.indexOf(next_sibling.tagName) <= levels.indexOf(sec_level)) {
               break
             }
+          }
+          if (next_sibling.tagName == 'DIV' && next_sibling.className.includes('ext-discussiontools-init-section')) { // wo yi ding yao sha le ni men
+            let child = next_sibling.firstChild
+            let bf = false
+            while(child){
+              if (levels.includes(child.tagName)){
+                if (levels.indexOf(child.tagName) <= levels.indexOf(sec_level)){
+                  bf = true
+                  break
+                }
+              }
+              child = child.nextSibling
+            }
+            if (bf){
+              break
+            }
+            
+
           }
           nbox.appendChild(next_sibling.cloneNode(true))
           next_sibling = next_sibling.nextSibling
